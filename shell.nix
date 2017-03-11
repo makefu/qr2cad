484ca524da5550b03ca67cb9751834f148ae93e3
makefu <github@syntax-fehler.de>
2017-03-11 20:02:11 +0100

    shell.nix: 2to3

--- shell.nix
+++ shell.nix
@@ -4,11 +4,10 @@ pkgs.stdenv.mkDerivation rec {
   name = "qr2cad-env";
   version = "1.1";
   buildInputs = with pkgs; [
-    openscad 
-    freecad
-    python2
-    python2Packages.pillow
-    python2Packages.qrcode
+    openscad
+    python3
+    python3Packages.pillow
+    python3Packages.qrcode
   ];
   shellHook = ''
     HISTFILE=$PWD/.histfile
